cd420e8e2df8feff652bd2c9280bd8c6f0e1c7d7
Volodymyr Bunin <volodymyr@xdesign.ws>
2018-05-01 01:22:35 +0300

    another switch

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -22,79 +22,80 @@ export default (state = initialState, { type, payload }) => {
 	switch(type){
 		case types.TOGGLE_CARD_CLICK:{
 			const { activeCards, cards } = state;
-
-			if(activeCards.length === 0){
-				console.log("1");
-				return {
-					...state,
-					activeCards: [...activeCards, payload],
-					cards: cards.map(el => 
-						el.id === payload.id 
-						? {
-							...el,
-							clicked: !el.clicked
-						} 
-						: el)
-				}
-			}else if(activeCards.length === 1){
-				if(activeCards[0].id === payload.id){
-					console.log("2");
+			switch(activeCards.length){
+				case 0:{
+					console.log("1");
 					return {
 						...state,
-						activeCards: [],
+						activeCards: [...activeCards, payload],
+						cards: cards.map(el => 
+							el.id === payload.id 
+							? {
+								...el,
+								clicked: !el.clicked
+							} 
+							: el)
+					}
+				}
+				case 1:{
+					if(activeCards[0].id === payload.id){
+						console.log("2");
+						return {
+							...state,
+							activeCards: [],
+							cards: cards.map(el => el.id === payload.id ? {
+								...el, 
+								clicked: false
+							} : el)
+						}
+					}
+					if(activeCards[0].color === payload.color){
+						console.log("3");
+						return {
+							...state,
+							activeCards: [],
+							cards: cards.map(el => 
+								el.id === activeCards[0].id ||
+								el.id === payload.id 
+									? {
+										...el,
+										clicked: false,
+										active: false
+									} : el)
+						}
+					}
+					console.log("4");
+					return {
+						...state,
+						activeCards: [...activeCards, payload],
 						cards: cards.map(el => el.id === payload.id ? {
-							...el, 
-							clicked: false
+							...el,
+							clicked: true
 						} : el)
 					}
 				}
-				if(activeCards[0].color === payload.color){
-					console.log("3");
+				case 2:{
+					if(activeCards.some(el => el.id === payload.id)){
+						console.log("5");
+						return {
+							...state,
+							activeCards: [],
+							cards: cards.map(el => 
+								activeCards.some(ell => ell.id === el.id)
+								? { ...el, clicked: false } : el)
+						}
+					}
+					console.log("6");
 					return {
-						...state,
-						activeCards: [],
+						activeCards: [payload],
 						cards: cards.map(el => 
-							el.id === activeCards[0].id ||
 							el.id === payload.id 
-								? {
-									...el,
-									clicked: false,
-									active: false
-								} : el)
+								? { ...el, clicked: true } 
+								: {...el, clicked: false }
+						)
 					}
-				}
-				console.log("4");
-				return {
-					...state,
-					activeCards: [...activeCards, payload],
-					cards: cards.map(el => el.id === payload.id ? {
-						...el,
-						clicked: true
-					} : el)
-				}
-			}else if(activeCards.length === 2){
-				if(activeCards.some(el => el.id === payload.id)){
-					console.log("5");
-					return {
-						...state,
-						activeCards: [],
-						cards: cards.map(el => 
-							activeCards.some(ell => ell.id === el.id)
-							? { ...el, clicked: false } : el)
-					}
-				}
-				console.log("6");
-				return {
-					activeCards: [payload],
-					cards: cards.map(el => 
-						el.id === payload.id 
-							? { ...el, clicked: true } 
-							: {...el, clicked: false }
-					)
 				}
 			}
-
-			
 		}
 		default:
 			return state;
